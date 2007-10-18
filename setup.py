28b90cbf3492d0fecb36951751edeabc8ea3e7e4
astraw <astraw@9608957c-362e-0410-90bb-ddb82fccb57f>
2007-10-18 18:32:24 +0000

    bump version number
    git-svn-id: https://code.astraw.com/motmot/trunk/flytrax@163 9608957c-362e-0410-90bb-ddb82fccb57f

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='flytrax',
       description='plugin for fview to perform 2D image tracking (part of the motmot camera packages)',
-      version='0.5.1',
+      version='0.5.2',
       license='BSD',
       author='Andrew Straw',
       author_email='strawman@astraw.com',
