c8e699282f0ff304f69d697118a46483d8e0e77d
Andrew Straw <strawman@astraw.com>
2010-09-16 22:37:37 -0700

    allow ROI2 radius to be set from GUI. change default tracking values

--- motmot/flytrax/flytrax.py
+++ motmot/flytrax/flytrax.py
@@ -510,7 +510,7 @@ class Tracker(trax_udp_sender.UDPSender):
 
         self.ticks_since_last_update[cam_id] = 0
         lbrt = (0,0,max_width-1,max_height-1)
-        roi2_radius=61
+        roi2_radius=15
         self.max_num_points[cam_id]=SharedValue()
         self.max_num_points[cam_id].set(8)
         ra = realtime_image_analysis.RealtimeAnalyzer(lbrt,
@@ -540,6 +540,20 @@ class Tracker(trax_udp_sender.UDPSender):
         ctrl.SetValue(str(self.max_num_points[cam_id].get_nowait()))
         validator.set_state('valid')
 
+
+        ctrl = xrc.XRCCTRL(per_cam_panel,"ROI2_RADIUS")
+        self.widget2cam_id[ctrl]=cam_id
+        validator = wxvt.setup_validated_integer_callback(
+            ctrl,
+            ctrl.GetId(),
+            self.OnROI2Radius,
+            ignore_initial_value=True)
+        self.xrcid2validator[cam_id]["ROI2_RADIUS"] = validator
+        ctrl.SetValue(str(roi2_radius))
+        validator.set_state('valid')
+
+        ra.clear_threshold = 0.8
+        ra.diff_threshold = 30
         self.clear_threshold_value[cam_id] = ra.clear_threshold
         self.clear_threshold_value[cam_id] = ra.diff_threshold
 
@@ -824,6 +838,19 @@ class Tracker(trax_udp_sender.UDPSender):
             pass
         else:
             self.max_num_points[cam_id].set( newval )
+        event.Skip()
+
+    def OnROI2Radius(self,event):
+        widget = event.GetEventObject()
+        cam_id = self.widget2cam_id[widget]
+
+        newvalstr = widget.GetValue()
+        try:
+            newval = int(newvalstr)
+        except ValueError:
+            pass
+        else:
+            self.realtime_analyzer[cam_id].roi2_radius = newval
         event.Skip()
 
     def process_frame(self,cam_id,buf,buf_offset,timestamp,framenumber):
